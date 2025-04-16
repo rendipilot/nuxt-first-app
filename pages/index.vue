85106fae97abf0d426f08d344fa32cf072bcef07
rendipilot <linahonduras@protonmail.com>
2025-04-16 10:22:55 +0700

    sample get random cat from cat api

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,21 @@
     <NuxtLink to="/about">
         <button class="flex items-center p-4 rounded-md bg-orange-400 mt-4 h-12">About page</button>
     </NuxtLink>
-    <button>Get Random Cat</button>
+    <button class="lex items-center mt-10 bg-blue-400 rounded-md p-4" @click="getRandomImage">Get Random Cat</button>
+    <img :src="catImage" alt="Random Cat" class="w-64 ml-10 rounded-md mt-10"/>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+
+const catImage = ref("")
+
+const getRandomImage = async() => {
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search")
+
+    catImage.value = res.data[0].url
+    console.log(catImage.value)
+}
 </script>
