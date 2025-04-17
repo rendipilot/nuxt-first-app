8ccfe906388fc08cdbf43c1ea32e6e35831b0b28
rendipilot <linahonduras@protonmail.com>
2025-04-17 10:08:18 +0700

    add pinia to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,16 +6,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
   app: {
     head: {
       // update Nuxt defaults
       charset: 'utf-16',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     },
-  }
+  },
+
+  modules: ['@pinia/nuxt']
 })
