0258171eb52340116f7b531dac551aa5624e1b52
rendipilot <linahonduras@protonmail.com>
2025-04-17 10:21:48 +0700

    add custom error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { clearError } from '#app'
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+  error: NuxtError
+}>()
+
+const handleError = () => {
+  clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+  <div>
+    <h2>{{ error.statusCode }}</h2>
+    <p>{{ error.message }}</p>
+    <button @click="handleError">Kembali ke Beranda</button>
+  </div>
+</template>
